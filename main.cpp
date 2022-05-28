794555f46a139178b4af840b7cae6fba70dafb60
Daniel Mattila <danmattila99@gmail.com>
2022-05-28 15:14:30 -0500

    Set window Icon

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     loginpage w;
+    w.setWindowIcon(QIcon("/usr/share/qt6_login_page/logo.png"));
     w.show();
     return QApplication::exec();
 }
